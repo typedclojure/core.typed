e0beda3b15867a32117b12dc4b9ccac397a549ae
Di Xu <xudifsd@gmail.com>
2014-07-01 13:55:30 +0800

    add pdot in frees of Function

--- module-check/src/main/clojure/clojure/core/typed/frees.clj
+++ module-check/src/main/clojure/clojure/core/typed/frees.clj
@@ -336,7 +336,7 @@
   (apply combine-frees (mapv frees types)))
 
 (add-frees-method [::frees Function]
-  [{:keys [dom rng rest drest kws prest]}]
+  [{:keys [dom rng rest drest kws prest pdot]}]
   (apply combine-frees (concat (mapv (comp flip-variances frees)
                                      (concat dom
                                              (when rest
@@ -348,10 +348,13 @@
                                [(frees rng)]
                                (when drest
                                  [(dissoc (-> (:pre-type drest) frees flip-variances)
-                                          (:name drest))]))))
+                                          (:name drest))])
+                               (when pdot
+                                 [(dissoc (-> (:pre-type pdot) frees flip-variances)
+                                          (:name pdot))]))))
 
 (add-frees-method [::idxs Function]
-  [{:keys [dom rng rest drest kws prest]}]
+  [{:keys [dom rng rest drest kws prest pdot]}]
   (apply combine-frees (concat (mapv #(-> % frees flip-variances)
                                      (concat dom
                                              (when rest
@@ -363,6 +366,12 @@
                                [(frees rng)]
                                (when drest
                                  (let [{:keys [name pre-type]} drest]
+                                   (assert (symbol? name))
+                                   [(t/ann-form {name :contravariant} VarianceMap)
+                                    (-> pre-type
+                                      frees flip-variances)]))
+                               (when pdot
+                                 (let [{:keys [name pre-type]} pdot]
                                    (assert (symbol? name))
                                    [(t/ann-form {name :contravariant} VarianceMap)
                                     (-> pre-type
