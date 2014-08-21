32a1f2cb75105b3693d418551823fbe86fc75d61
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2014-08-22 06:41:49 +0800

    This arity doesn't look correct.

--- module-check/src/main/clojure/clojure/core/typed/base_env.clj
+++ module-check/src/main/clojure/clojure/core/typed/base_env.clj
@@ -462,14 +462,14 @@ clojure.core/disj
 
 clojure.core/assoc
      (All [m k v c ...]
-          (Fn [m k v (HSeq [c c] :repeat true) <... c
-               -> (Assoc m k v c ... c)]
-            [m k v (HSeq [k v] :repeat true) <*
-             -> (Assoc m k v)]
-            [nil k v (HSeq [c c] :repeat true) <... c
-             -> (Assoc nil k v c ... c)]
-            [nil k v (HSeq [k v] :repeat true) <*
-             -> (Map k v)]))
+          (IFn [m k v (t/HSeq [c c] :repeat true) <... c
+                -> (Assoc m k v c ... c)]
+;               [m k v (t/HSeq [k v] :repeat true) <*
+;                -> (Assoc m k v)]
+               [nil k v (t/HSeq [c c] :repeat true) <... c
+                -> (Assoc nil k v c ... c)]
+               [nil k v (t/HSeq [k v] :repeat true) <*
+                -> (Map k v)]))
 ;     (All [b c d]
 ;       (Fn [(Map b c) b c -> (Map b c)]
 ;           [(Vec d) AnyInteger d -> (Vec d)]
