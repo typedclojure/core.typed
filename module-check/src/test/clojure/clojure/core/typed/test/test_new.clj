30ba0b280c15e7b01c12e5ad3b333e3325f4691c
Aravind K N <aravikn@gmail.com>
2015-06-21 09:58:52 +0530

    Create test_new.clj

---
+++ module-check/src/test/clojure/clojure/core/typed/test/test_new.clj
@@ -0,0 +1,12 @@
+(ns clojure.core.typed.test.test
+  (:require  [clojure.core.typed :as t] 
+             [clojure.test :refer :all]                
+             [clojure.core.typed.test.test-utils :refer :all]))
+
+(deftest function?-test
+  (is-tc-e   (function? function?) Boolean
+             :requires [[clojure.test :refer [function?]]])
+  (is-tc-err   (function? function?) String
+             :requires [[clojure.test :refer [function?]]])
+  (is-tc-err   (function? function) Boolean
+             :requires [[clojure.test :refer [function?]]]))
