4e922dfcb15e790874af61b52cabe06b7632e2a5
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2015-06-24 13:16:09 +0800

    WIP tests for Not type

--- module-check/src/test/clojure/clojure/core/typed/test/core.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/core.clj
@@ -4815,6 +4815,40 @@
   (is-tc-e (if (seq [1 2 3]) 1 nil)
            Num))
 
+
+(defmacro negate-sub [s t]
+  `(In (parse-type '~s) 
+       (make-Not (parse-type '~t))))
+
+(deftest not-test
+  (is-clj (= (parse-clj '(Not Int))
+             (parse-clj '(Not (Not (Not Int))))))
+  (is-clj (= (negate-sub Int Num)
+             -nothing))
+  (is-clj (= (negate-sub Num Int)
+             (parse-clj 'Int)))
+  (is-clj (= (clj (unp (negate-sub Num Int)))
+             (parse-clj 'Int))))
+
+;(I Num (Not Int))
+;
+;(I Num (Not Int))
+;
+;(clj (unp (negate-sub (U nil Num) Int)))
+;(clj (unp (negate-sub Int Num)))
+;(clj (unp (negate-sub (U nil Int) Num)))
+;
+;(deftest negate-sub-test
+;  (is-clj (negate-sub (HMap :mandatory {:a Int})
+;                      (HMap :mandatory {:a Num}))))
+;
+;(clj (negate-sub Int Num))
+;(clj (negate-sub Num Int))
+;
+;(clj (unparse-type
+;       (In (parse-clj 'Num)
+;           (parse-clj '(Not Num)))))
+;
 ;    (is-tc-e 
 ;      (let [f (fn [{:keys [a] :as m} :- '{:a (U nil Num)}] :- '{:a Num} 
 ;                {:pre [(number? a)]} 
