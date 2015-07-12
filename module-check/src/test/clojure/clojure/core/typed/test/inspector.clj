fd9db3023960192a6c5ef5a2b69e6831a96c39f0
Aravind K N <aravikn@gmail.com>
2015-07-12 18:03:25 +0530

    Update inspector.clj

--- module-check/src/test/clojure/clojure/core/typed/test/inspector.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/inspector.clj
@@ -1,8 +1,7 @@
 (ns clojure.core.typed.test.inspector
   (:require  [clojure.core.typed :as t] 
              [clojure.test :refer :all]                
-             [clojure.core.typed.test.test-utils :refer :all]
-             [clojure.core.typed.test.destructure]))
+             [clojure.core.typed.test.test-utils :refer :all]))
 
 (deftest atom?-test
   (is-tc-e (atom? "abc" ) Boolean
