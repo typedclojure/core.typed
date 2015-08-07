bfe07d1292c153670e5279230c105d50d4ab8388
Aravind K N <aravikn@gmail.com>
2015-08-07 23:52:04 +0530

    Update mainnew.clj

--- module-check/src/test/clojure/clojure/core/typed/test/mainnew.clj
+++ module-check/src/test/clojure/clojure/core/typed/test/mainnew.clj
@@ -22,5 +22,33 @@
 (deftest main-test
   (is-tc-e #(main) [-> nil] 
              :requires [[clojure.main :refer [main]]]))
+           
+(deftest load-script-test
+  (is-tc-e #(load-script "sample.clj" ) [-> Any] 
+             :requires [[clojure.main :refer [load-script]]])
+  (is-tc-err (load-script 1 ) Any
+             :requires [[clojure.main :refer [load-script]]]))
+
+(deftest repl-caught-test
+  (is-tc-e (repl-caught (Exception. "a")) Any 
+             :requires [[clojure.main :refer [repl-caught]]])
+  (is-tc-err (repl-caught "A") Any
+             :requires [[clojure.main :refer [repl-caught]]]))
+
+(deftest repl-exception-test
+  (is-tc-e (repl-exception (Exception. "a")) Any 
+             :requires [[clojure.main :refer [repl-exception]]])
+  (is-tc-err (repl-exception "A") Any
+             :requires [[clojure.main :refer [repl-exception]]]))
+
+(deftest root-cause-test
+  (is-tc-e (root-cause (Exception. "a")) Exception 
+             :requires [[clojure.main :refer [root-cause]]])
+  (is-tc-err (root-cause "A") Exception
+             :requires [[clojure.main :refer [root-cause]]])
+  (is-tc-err (root-cause (Exception. "a")) String
+             :requires [[clojure.main :refer [root-cause]]]))
+
+
 
 
