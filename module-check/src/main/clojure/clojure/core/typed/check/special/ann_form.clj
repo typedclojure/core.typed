5b8eb4234f3afab6ebaf51f84595f05b51273bc4
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2016-07-06 15:32:00 -0400

    ann-form

--- module-check/src/main/clojure/clojure/core/typed/check/special/ann_form.clj
+++ module-check/src/main/clojure/clojure/core/typed/check/special/ann_form.clj
@@ -17,13 +17,11 @@
   [{:keys [statements] :as expr}]
   (let [[_ _ texpr] statements
         tsyn-quoted (ast-u/map-expr-at texpr :type)
-        _ (impl/impl-case
-            :clojure (assert (and (seq? tsyn-quoted)
-                                  ('#{quote} (first tsyn-quoted)))
-                             tsyn-quoted)
-            :cljs nil)
         tsyn (impl/impl-case
-               :clojure (second tsyn-quoted)
+               :clojure (if (seq? tsyn-quoted)
+                          ;; unquote
+                          (second tsyn-quoted)
+                          tsyn-quoted)
                :cljs tsyn-quoted)]
     tsyn))
 
