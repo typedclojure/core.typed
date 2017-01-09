3f0d54f749bbaff8bf17e5dcfdd9010cc33a7a2f
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2017-01-09 10:26:47 -0500

    Convert non-nil return env to global atom

--- module-check/src/main/clojure/clojure/core/typed/method_return_nilables.clj
+++ module-check/src/main/clojure/clojure/core/typed/method_return_nilables.clj
@@ -1,26 +1,31 @@
 (ns clojure.core.typed.method-return-nilables
   (:require [clojure.core.typed.contract-utils :as con]
+            [clojure.core.typed.env :as env]
             [clojure.core.typed.type-rep :as r]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Method Return non-nilables
 
-(defonce METHOD-RETURN-NONNILABLE-ENV 
-  (atom {}
-        :validator (con/hash-c? (every-pred namespace symbol?)
-                                (some-fn #(= :all %)
-                                         (con/set-c? con/znat?)))))
+(def method-return-nonnilable-env-kw ::method-return-nonnilable-env)
 
 (defn add-nonnilable-method-return [sym m]
-  (swap! METHOD-RETURN-NONNILABLE-ENV assoc sym m)
+  {:pre [((every-pred namespace symbol?) sym)
+         ((some-fn #(= :all %)
+                   (con/set-c? con/znat?))
+          m)]}
+  (env/swap-checker! assoc-in [method-return-nonnilable-env-kw sym] m)
   nil)
 
 (defn reset-nonnilable-method-return-env! [m]
   (reset! METHOD-RETURN-NONNILABLE-ENV m)
   nil)
 
+(defn nonnilable-method-return-env [sym m]
+  {:post [(map? %)]}
+  (get (env/deref-checker) method-return-nonnilable-env-kw {}))
+
 (defn nonnilable-return? [sym arity]
-  (let [as (@METHOD-RETURN-NONNILABLE-ENV sym)]
+  (let [as (get (nonnilable-method-return-env) sym)]
     (boolean (or (= :all as)
                  (when as
                    (as arity))))))
