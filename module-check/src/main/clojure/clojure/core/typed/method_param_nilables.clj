4170490dfdd8cbfd8a5073f70a4ad07ff69a9be4
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2017-01-09 10:36:37 -0500

    Move nilable-param env to global atom

--- module-check/src/main/clojure/clojure/core/typed/method_param_nilables.clj
+++ module-check/src/main/clojure/clojure/core/typed/method_param_nilables.clj
@@ -1,22 +1,23 @@
 (ns clojure.core.typed.method-param-nilables
   (:require [clojure.core.typed.contract-utils :as con]
+            [clojure.core.typed.env :as env]
             [clojure.core.typed.type-rep :as r]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Method Param nilables
 
-(defonce METHOD-PARAM-NILABLE-ENV 
-  (atom {}
-        :validator (con/hash-c? (every-pred namespace symbol?)
-                                (con/hash-c? (some-fn #{:all} con/znat?)
-                                             (some-fn #{:all} (con/set-c? con/znat?))))))
+(def method-param-nilable-env-kw ::method-param-nilable-env)
 
 (defn reset-method-nilable-param-env! [m]
   (reset! METHOD-PARAM-NILABLE-ENV m)
   nil)
 
 (defn add-method-nilable-param [sym a]
-  (swap! METHOD-PARAM-NILABLE-ENV assoc sym a)
+  {:pre [((every-pred namespace symbol?) sym)
+         ((con/hash-c? (some-fn #{:all} con/znat?)
+                       (some-fn #{:all} (con/set-c? con/znat?)))
+          a)]}
+  (env/swap-checker! assoc-in [method-param-nilable-env-kw sym] a)
   nil)
 
 (defn nilable-param? [sym arity param]
