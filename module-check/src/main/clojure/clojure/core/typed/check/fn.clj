4a661138884887fcc03b546023a97f698df7ebdb
Ambrose Bonnaire-Sergeant <abonnairesergeant@gmail.com>
2018-05-02 16:44:21 -0400

    fn methods

--- module-check/src/main/clojure/clojure/core/typed/check/fn.clj
+++ module-check/src/main/clojure/clojure/core/typed/check/fn.clj
@@ -8,8 +8,6 @@
             [clojure.core.typed.check.fn-methods :as fn-methods]
             [clojure.core.typed.check.utils :as cu]))
 
-(alter-meta! *ns* assoc :skip-wiki true)
-
 ; Can take a CLJ or CLJS function expression.
 ;
 ;[FnExpr (Option Type) -> Expr]
